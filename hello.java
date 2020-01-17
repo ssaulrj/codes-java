5f115a77b417aeb5af5cd209189d485f7a841c4d
ssaulrj <59984185+ssaulrj@users.noreply.github.com>
2020-01-16 18:48:35 -0600

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello pata!");
+        Main2 hola = new Main2();
+        hola.main();
+    }
+}
+
+// The main method must be in a class named "Main".
+class Main2 {
+    public static void main() {
+        System.out.println("Love u so much!");
+    }
+}
